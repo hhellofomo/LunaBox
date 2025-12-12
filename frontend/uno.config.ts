324bdcc3fc24a9cfac748c3dde2e6443bee6a3fb
Vivian-Roy <233558647+Vivian-Roy@users.noreply.github.com>
2025-12-12 15:36:14 +0800

    chore: update ts version and introduce icons

--- frontend/uno.config.ts
+++ frontend/uno.config.ts
@@ -1,8 +1,10 @@
 import presetWind3 from '@unocss/preset-wind3'
+import presetIcons from '@unocss/preset-icons'
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
     presets: [
         presetWind3(),
+        presetIcons(),
     ],
 })
