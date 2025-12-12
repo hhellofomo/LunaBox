a5f6ae98a257e3e7359543c470e7a253c6d7c903
hhellofomo <191084278+hhellofomo@users.noreply.github.com>
2025-12-12 13:08:02 +0800

    fix: update bound wails method

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -33,24 +33,6 @@ export namespace enums {
 
 export namespace models {
 	
-	export class Category {
-	    id: string;
-	    user_id: string;
-	    name: string;
-	    is_system: boolean;
-	
-	    static createFrom(source: any = {}) {
-	        return new Category(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.id = source["id"];
-	        this.user_id = source["user_id"];
-	        this.name = source["name"];
-	        this.is_system = source["is_system"];
-	    }
-	}
 	export class Game {
 	    id: string;
 	    user_id: string;
@@ -171,6 +153,50 @@ export namespace vo {
 	        if ('string' === typeof source) source = JSON.parse(source);
 	        this.chat_ids = source["chat_ids"];
 	    }
+	}
+	export class CategoryVO {
+	    id: string;
+	    user_id: string;
+	    name: string;
+	    is_system: boolean;
+	    // Go type: time
+	    created_at: any;
+	    // Go type: time
+	    updated_at: any;
+	    game_count: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new CategoryVO(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.user_id = source["user_id"];
+	        this.name = source["name"];
+	        this.is_system = source["is_system"];
+	        this.created_at = this.convertValues(source["created_at"], null);
+	        this.updated_at = this.convertValues(source["updated_at"], null);
+	        this.game_count = source["game_count"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
 	}
 	export class DailyPlayTime {
 	    date: string;
